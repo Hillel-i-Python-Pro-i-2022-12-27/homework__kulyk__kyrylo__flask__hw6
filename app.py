bd9f4920d962a38dad32d6b3d0e6fa6384da2a6b
ptizaS <ptizay@gmail.com>
2023-01-26 03:21:17 +0100

    ✨ Application (completed homework 6)

--- app.py
+++ app.py
@@ -13,43 +13,37 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    profile_1 = "/read_file"
-    profile_2 = "/users"
-    profile_3 = "/astronauts"
-    profile_4 = "/calculate"
-    return f"""
+def hello_world() -> str:
+    return """
     <title>Flask Application</title>
     <h1>Homework tasks:</h1>
-    <a href={profile_1} class="page-to">Task 1 - Reading file. »
+    <a href=/read_file class="page-to">Task 1 - Reading file. »
     <h2></h2>
-    <a href={profile_2} class="page-to">Task 2 - List of users. »
+    <a href=/users class="page-to">Task 2 - List of users. »
     <h2></h2>
-    <a href={profile_3} class="page-to">Task 3 - List of astronauts. »
+    <a href=/astronauts class="page-to">Task 3 - List of astronauts. »
     <h2></h2>
-    <a href={profile_4} class="page-to">Task 4 - Calculate average values from CSV-file. »
+    <a href=/calculate class="page-to">Task 4 - Calculate average values from CSV-file. »
     """
 
 
 @app.route("/read_file")
-def to_read_fila_txt():
+def to_read_fila_txt() -> str:
     to_create_file_txt(name_file="fake_text")
     read_file = to_read_file_txt(name_file="fake_text")
-    profile = "/"
     return f"""
     <h2>Text from fake_text.txt:</h2>
     <span>{read_file}</span>
     <h3></h3>
-    <a href={profile} class='page-to'>To the main »
+    <a href=/ class='page-to'>To the main »
     """
 
 
 @app.route("/users")
 @use_args({"amount": fields.Int(missing=10)}, location="query")
-def to_generate_users(args):
+def to_generate_users(args) -> str:
     amount = args["amount"]
     users = generate_users(amount=amount)
-    profile = "/"
     output = "".join(
         f"<li>" f"<span>{User.name}</span>" f"<span> - </span>" f"<span>{User.email}</span>" f"</li>" for User in users
     )
@@ -57,36 +51,34 @@ def to_generate_users(args):
     <h3>Users:</h3>
     <ol>{output}</ol>
     <h3></h3>
-    <a href={profile} class='page-to'>To the main »
+    <a href=/ class='page-to'>To the main »
     """
 
 
 @app.route("/astronauts")
-def to_get_info_astronauts():
+def to_get_info_astronauts() -> str:
     to_requests_data(url="http://api.open-notify.org/astros.json")
-    profile = "/"
     return f"""
     <h3>List of astronauts:</h3>
     <span>The number of people in space at this moment:</span>
     <h3></h3>
     <span>{to_processing_response(name_file="output")}-astronauts</span>
     <h3></h3>
-    <a href={profile} class='page-to'>To the main »
+    <a href=/ class='page-to'>To the main »
     """
 
 
 @app.route("/calculate")
-def to_get_average_values():
-    to_download_file("https://drive.google.com/u/0/uc?id=1kQ8mFcgGpGK4XRtnWQdWf-y10Ru2UQdB&export=download")
+def to_get_average_values() -> str:
+    to_download_file(url="https://drive.google.com/u/0/uc?id=1kQ8mFcgGpGK4XRtnWQdWf-y10Ru2UQdB&export=download")
     average_values = to_calculation_average(name_file="output")
-    profile = "/"
     return f"""
     <h3>After processing the csv-file, we got the following values:</h3>
     <span>Average height of people: {average_values["average_height_in_cm"]} cm.</span>
     <h3></h3>
     <span>Average weight of people: {average_values["average_weight_in_kg"]} kg.</span>
     <h3></h3>
-    <a href={profile} class='page-to'>To the main »
+    <a href=/ class='page-to'>To the main »
     """
 
 
